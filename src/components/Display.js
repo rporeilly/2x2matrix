138918639371a45002d501d79277a137d97c883e
Ryan O'Reilly <roreilly@pointb.com>
2021-09-01 14:36:33 -0700

    Adding framer motion

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,25 +3,35 @@ import {
 } from '@chakra-ui/react';
 import {ArrowForwardIcon} from '@chakra-ui/icons'
 import { useSelector } from 'react-redux'
+import { motion } from "framer-motion"
 
 function Display(props) {
 
   const options = useSelector(state => state)
   const optionsObject = [...Object.values(options), ...Object.keys(options)]
   // console.log(optionsObject[0])
+  const fadeInSpeed = 0.5
 
   // console.log("Display: " + options.option[0].name)
   const listItemsTopLeft = optionsObject[0].filter(effortType => effortType.effort === '12').map((i) =>
-    <ListItem key={i.index}>{i.name}</ListItem>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: fadeInSpeed }} >
+        <ListItem key={i.index}>{i.name}</ListItem>
+    </motion.div>
   )
   const listItemsTopRight = optionsObject[0].filter(effortType => effortType.effort === '22').map((i) =>
-    <ListItem key={i.index}>{i.name}</ListItem>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: fadeInSpeed }} >
+      <ListItem key={i.index}>{i.name}</ListItem>
+    </motion.div>
   )
   const listItemsBottomLeft = optionsObject[0].filter(effortType => effortType.effort === '11').map((i) =>
-    <ListItem key={i.index}>{i.name}</ListItem>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: fadeInSpeed }} >
+      <ListItem key={i.index}>{i.name}</ListItem>
+    </motion.div>
   )
   const listItemsBottomRight = optionsObject[0].filter(effortType => effortType.effort === '21').map((i) =>
-    <ListItem key={i.index}>{i.name}</ListItem>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: fadeInSpeed }} >
+      <ListItem key={i.index}>{i.name}</ListItem>
+    </motion.div>
   )
   return (
     <Box position="relative">
@@ -32,28 +42,28 @@ function Display(props) {
       </Box>
       <Box rounded="md" boxShadow="md">
         <SimpleGrid columns={2} spacing={0}>
-          <Box bg="#ffff76">
+          <Box minH="200" bg="#ffff76">
             <Center p="10">
               <UnorderedList>
                 {listItemsTopLeft}
               </UnorderedList>
             </Center>
           </Box>
-          <Box bg="#b4e0b4">
+          <Box minH="200" bg="#b4e0b4">
             <Center p="10">
               <UnorderedList>
                 {listItemsTopRight}
               </UnorderedList>
             </Center>
           </Box>
-          <Box bg="#f1a8a8">
+          <Box minH="200" bg="#f1a8a8">
             <Center p="10">
               <UnorderedList>
                 {listItemsBottomLeft}
               </UnorderedList>
             </Center>
           </Box>
-          <Box bg="#f3d89d">
+          <Box minH="200" bg="#f3d89d">
             <Center p="10">
               <UnorderedList>
                 {listItemsBottomRight}
